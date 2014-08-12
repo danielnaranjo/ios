a14c6a14978d8acf089c35f7e290ee0f5e327f45
Daniel Naranjo <dnaranjo@gmail.com>
2014-08-12 17:20:47 -0430

    Initial Commit

---
+++ WebView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WebView
+//
+//  Created by Daniel Naranjo on 8/12/14.
+//  Copyright (c) 2014 Daniel Naranjo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WebView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WebView
+//
+//  Created by Daniel Naranjo on 8/12/14.
+//  Copyright (c) 2014 Daniel Naranjo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
